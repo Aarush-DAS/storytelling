f219e9432dafbc6e0abd0cc870acb477baaaee68
Aarush-DAS <78961766+Aarush-DAS@users.noreply.github.com>
2022-11-30 18:23:00 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCh0MfGRvUEod6KdWFmEL7acmY45v2npfE",
+  authDomain: "project-4988046057906262289.firebaseapp.com",
+  projectId: "project-4988046057906262289",
+  storageBucket: "project-4988046057906262289.appspot.com",
+  messagingSenderId: "262434542624",
+  databaseURL:"https://project-4988046057906262289-default-rtdb.firebaseio.com",
+  appId: "1:262434542624:web:3dec632c3da90300981845"
+  };
+
